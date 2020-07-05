b2e5338b18797b923b49a7b457a08917aacd7f4f
Duosora <ergi.duoson@gmail.com>
2020-07-05 14:41:49 +0300

    Updates

---
+++ precache-manifest.2a3b5c356dbb5681225def53be9f2153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f262a05320cceed98939acfec3d779b",
+    "url": "/dgaspv4/index.html"
+  },
+  {
+    "revision": "d3e39f9dac4cff23100d",
+    "url": "/dgaspv4/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2efe9b5ff97634c37690",
+    "url": "/dgaspv4/static/css/main.a8872e21.chunk.css"
+  },
+  {
+    "revision": "d3e39f9dac4cff23100d",
+    "url": "/dgaspv4/static/js/2.fdbe642d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dgaspv4/static/js/2.fdbe642d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efe9b5ff97634c37690",
+    "url": "/dgaspv4/static/js/main.5060c572.chunk.js"
+  },
+  {
+    "revision": "f6423da7370aa0d23492",
+    "url": "/dgaspv4/static/js/runtime-main.5ddc8ed7.js"
+  }
+]);
