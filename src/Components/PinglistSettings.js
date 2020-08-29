4cdc70b6c99dcec456da1b98260c9012fe518e5c
Duosora <ergi.duoson@gmail.com>
2020-08-29 13:59:18 +0300

    Code message

--- src/Components/PinglistSettings.js
+++ src/Components/PinglistSettings.js
@@ -7,7 +7,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 class PinglistSettings extends React.Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			selectedBreed		 : 0,
 			selectedType		 : 0,
@@ -16,21 +16,21 @@ class PinglistSettings extends React.Component {
 			currentPinglist	 : ''
 		}
 	}
-	
+
 	updateFilter = (filterKey,filterValue) => {
 		this.setState({[filterKey]: filterValue, currentPinglist: ''});
 	}
-	
+
 	onChangeHandler = (event) => {
 		const filterName  = event.target.name;
 		const filterValue = event.target.value;
-		
+
 		this.updateFilter(filterName,filterValue);
 	}
-	
+
 	inArray(whichArray,whichValue,deep=false) {
 		let finalResult = whichArray.includes(whichValue);
-		
+
 		// If the value is found, there is no need for deep search.
 		if(!finalResult) {
 			if(deep) {
@@ -43,34 +43,34 @@ class PinglistSettings extends React.Component {
 				});
 			}
 		}
-		
+
 		return finalResult;
 	}
-	
+
 	isGenerationAllowed = () => {
 		return 		(!!this.state.selectedBreed)
 					 && (!!this.state.selectedType)
 					 && (!!this.state.selectedResell)
 					 && (this.state.selectedKeywords.length > 0);
 	}
-	
+
 	updateKeywords = (selectedKeywords) => {
 		this.updateFilter('selectedKeywords',Array.from(selectedKeywords));
 	}
-	
+
 	componentDidMount = () => {
 		this.onChangeHandler			= this.onChangeHandler.bind(this);
 		this.updateFilter					= this.updateFilter.bind(this);
 		this.updateKeywords 			= this.updateKeywords.bind(this);
 		this.isGenerationAllowed	= this.isGenerationAllowed.bind(this);
-		
+
 		this.checkPinglistUser  	= this.checkPinglistUser.bind(this);
 		this.generatePinglist			= this.generatePinglist.bind(this);
 		this.userWantsPings				= this.userWantsPings.bind(this);
-		
+
 		this.inArray							= this.inArray.bind(this);
 	}
-	
+
 	// On executing this, there is a guarantee the user WANTS pings of some kind.
 	checkPinglistUser = (userData) => {
 		const userBreeds					 = userData[1].split(', '),
@@ -79,29 +79,29 @@ class PinglistSettings extends React.Component {
 					userUnwantedKeywords = (userData.length > 4) ? userData[4].split(', ') : [],
 					wantedKeywords			 = this.state.selectedKeywords.filter(e => !userUnwantedKeywords.includes(e)),
 					matchedKeywords			 = Array.from(wantedKeywords).filter(e => userKeywords.includes(e));
-		
+
 		// If wantedKeywords.length === 0, it means no keywords from filter request are wanted by user.
 		if(wantedKeywords.length === 0) {
 			return false;
 		}
-		
+
 		if(this.state.selectedResell === "Yes") {
 			// If the user doesn't want to be pinged for resells, filter them out now.
 			if(userUnwantedKeywords.includes('resell')) {
 				return false;
 			}
 		}
-		
+
 		// If the necessary breed option is absent in this user's preference, filter them out.
 		if(!userBreeds.includes(this.state.selectedBreed)) {
 			return false;
 		}
-		
+
 		// Same for type.
 		if(!userSkinAccent.includes(this.state.selectedType)) {
 			return false;
 		}
-		
+
 		// No preference on keywords + userUnwantedKeywords = user wants any keyword except the unwanted ones.
 		// Else the user wants *matching* keywords strictly
 		if(!userKeywords.includes('no preference')) {
@@ -109,14 +109,14 @@ class PinglistSettings extends React.Component {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	userWantsPings = (userName) => {
 		return !this.inArray(globalState.doNotPingList,userName,true);
 	}
-	
+
 	generatePinglist = () => {
 		const pinglistArray = [];
 
@@ -128,12 +128,12 @@ class PinglistSettings extends React.Component {
 				}
 			}
 		});
-		
-		let pinglistString = "[center][url=http://www1.flightrising.com/forums/skin/2480522]Please click here to go to the GASP thread![/url][br][br]Keywords selected: "+this.state.selectedBreed+', '+this.state.selectedKeywords.join(', ')+"[br][br]- Pings auto-hidden -[/center][br][size=0][size=0][size=0][size=0][size=0][color=transparent]"+pinglistArray.join(' ')+"[/color][/size][/size][/size][/size][/size]";
-			
+
+		let pinglistString = "[center][url=http://www1.flightrising.com/forums/skin/2480522]Please click here to go to the GASP thread![/url][br][br]Keywords selected: "+this.state.selectedBreed+', '+this.state.selectedKeywords.join(', ')+"[br][br]- Pings auto-hidden -[br]The GASP website was recently rewritten, so this pinglist was generated using entirely new code! [b]PLEASE[/b] be sure to let us know if something is off![/center][br][size=0][size=0][size=0][size=0][size=0][color=transparent]"+pinglistArray.join(' ')+"[/color][/size][/size][/size][/size][/size]";
+
 		this.setState({currentPinglist: pinglistString});
 	}
-	
+
 	render () {
 		return (
 			<Form>
@@ -154,7 +154,7 @@ class PinglistSettings extends React.Component {
 								}
 							</Input>
 						</Col>
-						
+
 						<Col>
 							<Input
 								type="select"
@@ -168,7 +168,7 @@ class PinglistSettings extends React.Component {
 								<option value="Skincents">Skincents</option>
 							</Input>
 						</Col>
-						
+
 						<Col>
 							<Input
 								type="select"
@@ -182,15 +182,15 @@ class PinglistSettings extends React.Component {
 							</Input>
 						</Col>
 					</Row>
-					
+
 					<h4>Keywords</h4>
-					
+
 					<Row>
 						<Col>
 							<KeywordForm onAdjust={this.updateKeywords} />
 						</Col>
 					</Row>
-					
+
 					<Row className="pinglistBtnsRow">
 						<Col>
 							<Button
@@ -203,14 +203,14 @@ class PinglistSettings extends React.Component {
 								Generate Pinglist
 							</Button>
 						</Col>
-						
+
 						<Col>
 							<CopyToClipboard text={this.state.currentPinglist}>
 								<Button className="CopyButton" color="success" size="lg">Copy Pinglist</Button>
 							</CopyToClipboard>
 						</Col>
 					</Row>
-					
+
 					<Row>
 						<Col>
 							<Input
