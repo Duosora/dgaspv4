769790ad580ecb4c7428650841c4733a0de34870
Duosora <ergi.duoson@gmail.com>
2020-08-29 11:25:36 +0300

    Technical message

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,12 @@ class Header extends React.Component {
 				<a rel="noopener noreferrer" target="_blank" href="http://www1.flightrising.com/forums/skin/2480522">
 					<img alt="General Accent & Skin Pinglists - Logo" src={logo} />
 				</a>
+
+				<br/><br/>
+
+				<p>
+					This application is in the process of active development. Please report any bugs or share your opinions on further improvement by messaging <a rel="noopener noreferrer" target="_blank" href="https://www1.flightrising.com/clan-profile/355822">Duosora</a> on Flight Rising.
+				</p>
 			</div>
 		);
 	}
